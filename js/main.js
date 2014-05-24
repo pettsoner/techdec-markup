6bcae739a8f27455e7036c1d39749677c833a218
Sergey <pettson@list.ru>
2014-05-24 18:20:51 +0400

    Реализован слайдер

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
-    
+    $('.b-leads__slider').bxSlider({
+        minSlides: 2,
+        mode: 'vertical',
+        slideMargin: 80,
+        pager: false,
+        nextText: 'Еще рынки',
+        prevText: 'Еще рынки'
+    }); 
 
 });
